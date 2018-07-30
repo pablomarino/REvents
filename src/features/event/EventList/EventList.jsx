be4aaf2c929c6e5ca70d26467f3374e81d7c25f2
Pablo <pablo.marino.boga@gmail.com>
2018-07-31 01:04:01 +0200

    Add Event Form

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import EventListItem from "./EventListItem";
 class EventList extends Component {
   render() {
+    const {events} = this.props;
     return (
       <div>
-        <EventListItem />
-        <EventListItem />
-        <EventListItem />
+        {events.map((event) => (
+          <EventListItem key={event.id} event={event}/>
+        ))}
       </div>
     );
   }
